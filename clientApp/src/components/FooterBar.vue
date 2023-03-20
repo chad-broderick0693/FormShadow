7598fa892e1f8654fa07d7f905e153d3ff8b5bc0
Chad Broderick <c.broderick0693@gmail.com>
2023-03-20 17:30:24 -0400

    feat: add Footer

---
+++ clientApp/src/components/FooterBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="footer">Powered by WuRM</section>
+</template>
+
+<style lang="scss">
+@import '@/assets/main.scss';
+
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: $color-primary;
+  color: $color-secondary;
+  font-size: 2rem;
+  width: 100%;
+  height: 5rem;
+}
+</style>
--- clientApp/src/views/HomeView.vue
+++ clientApp/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <HeaderBar />
+  <FooterBar />
 </template>
 
 <script setup>
 import HeaderBar from '@/components/HeaderBar.vue'
+import FooterBar from '@/components/FooterBar.vue'
 </script>
 
 <style lang="scss"></style>
