49df1aecd4c2106859fce72da892b26281a353ee
Chad Broderick <c.broderick0693@gmail.com>
2023-03-20 17:52:36 -0400

    feat: Create CustomButton

---
+++ clientApp/src/components/CustomButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button>Create Form Shadow</button>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/main.scss';
+
+button {
+  background-color: $color-primary;
+  color: $color-secondary;
+  font-family: 'Nunito';
+  font-size: 2rem;
+  outline: none;
+  border: none;
+  width: 75%;
+  max-width: 25rem;
+  padding: 1rem;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  user-select: none;
+  backface-visibility: hidden;
+  transition: all 0.05s linear;
+
+  &:active {
+    transform: translateY(1px) scale(0.99);
+  }
+
+  @media (min-width: 500px) {
+    &:hover {
+      box-shadow: 0px 0px 40px -15px rgba($color-secondary, 0.7);
+      transform: translateY(-2px) scale(1.01);
+    }
+
+    &:active {
+      box-shadow: 0px 0px 30px -15px rgba($color-secondary, 0.7);
+      transform: translateY(1px) scale(0.99);
+    }
+  }
+}
+</style>
--- clientApp/src/views/HomeView.vue
+++ clientApp/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
   <HeaderBar />
+  <CustomButton />
   <FooterBar />
 </template>
 
 <script setup>
-import HeaderBar from '@/components/HeaderBar.vue'
+import CustomButton from '@/components/CustomButton.vue'
 import FooterBar from '@/components/FooterBar.vue'
+import HeaderBar from '@/components/HeaderBar.vue'
 </script>
 
 <style lang="scss"></style>
